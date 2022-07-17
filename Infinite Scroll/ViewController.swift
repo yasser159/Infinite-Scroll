bb91dd6c0f0e5657dd6b66967f8d217150e391ca
Yasser Hajlaoui <yasser159@gmail.com>
2022-07-16 21:54:33 -0700

    Initial Commit

---
+++ Infinite Scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infinite Scroll
+//
+//  Created by Yasser Hajlaoui on 7/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
